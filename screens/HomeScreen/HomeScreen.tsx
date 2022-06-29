085a08e1abe1fc5f2da7caddc9a683e8cdb03702
VicenBayugar <vicentebayugar@gmail.com>
2022-06-29 12:55:54 -0300

    List of cryptocurrencies (hardcoded)

--- screens/HomeScreen/HomeScreen.tsx
+++ screens/HomeScreen/HomeScreen.tsx
@@ -5,8 +5,31 @@ import Cryptocurrencies from '../../components/Cryptocurrencies/Cryptocurrencies
 import {Container, AddCryptoButton} from './styles';
 
 const Home = ({navigation}) => {
-  const {cryptos} = useSelector(state => state.cryptos);
-
+  // const {cryptos} = useSelector(state => state.cryptos);
+  const cryptos = [
+    {
+      data: {
+        id: '5da7bde4-61e9-4a6f-9fc3-7ed66effb198',
+        symbol: 'BUSD',
+        name: 'Binance USD',
+        market_data: {
+          price_usd: 0.99998,
+          percent_change_usd_last_24_hours: 0.059,
+        },
+      },
+    },
+    {
+      data: {
+        id: '1e31218a-e44e-4285-820c-8282ee222035',
+        symbol: 'BTC',
+        name: 'Bitcoin',
+        market_data: {
+          price_usd: 20027.0629,
+          percent_change_usd_last_24_hours: -2.059,
+        },
+      },
+    },
+  ];
   return (
     <>
       <StatusBar backgroundColor={'#476280'} />
